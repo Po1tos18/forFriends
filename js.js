872737f8471e4b4eb13e57bb02957dd734c68a90
Nikita Tsyhankov <92795035+Po1tos18@users.noreply.github.com>
2025-05-17 20:48:56 +0300

    Update js.js

--- js.js
+++ js.js
@@ -1,4 +1,23 @@
 document.querySelector(".btn").addEventListener("click", function(){
     let name = document.querySelector(".textIn").value;
+    if(name == "Цыганков Никита Олегович"){
+    alert(`Ваше гейское имя: Крылов Максим`);
+    }else if(name == "Цыганков"){
+    alert(`Ваше гейское имя: Крылов Максим`);
+    }else if(name == "Циганков"){
+    alert(`Ваше гейское имя: Крылов Максим`);
+    }else if(name == "Никита"){
+    alert(`Ваше гейское имя: Крылов Максим`);
+    }else if(name == "Nikita"){
+    alert(`Ваше гейское имя: Крылов Максим`);
+    }else if(name == "цыганков"){
+    alert(`Ваше гейское имя: Крылов Максим`);
+    }else if(name == "циганков"){
+    alert(`Ваше гейское имя: Крылов Максим`);
+    }else if(name == "никита"){
+    alert(`Ваше гейское имя: Крылов Максим`);
+    }else{
     alert(`Ваше гейское имя: ${name}`);
+    }
+    
 })
